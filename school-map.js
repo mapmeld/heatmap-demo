1acf0452fcf17c55eef27d54deedd98dc8947070
Nick Doiron <ndoiron@mapmeld.com>
2019-07-12 09:25:19 +0900

    update school map

--- school-map.js
+++ school-map.js
@@ -7,17 +7,35 @@ function initMap() {
     //mapTypeId: 'satellite'
   });
 
-  fetch("robo_points.csv").then(res => res.text()).then((robos) => {
+  fetch("robo_points.csv?r=2").then(res => res.text()).then((robos) => {
     robos = robos.split("\n")
-      .filter((line) => (line.length > 4) && (line !== '0,0'))
+      .filter(line => (line.length > 4) && (line !== '0,0') && (line !== 'lat,lon,value'))
       .map((line) => {
-        let lat = line.split(',')[0] * 1 || 0,
-            lng = line.split(',')[1] * 1 || 0;
-        return new google.maps.LatLng(lat, lng);
+        let lat = (line.split(',')[0] * 1) || 0,
+            lng = (line.split(',')[1] * 1) || 0;
+        return [lat, lng]; //new google.maps.LatLng(lat, lng);
     });
+    //console.log(robos);
     heatmap = new google.maps.visualization.HeatmapLayer({
       data: robos,
       map: map
+    });
+
+    fetch("school_points.csv").then(res => res.text()).then((schools) => {
+      schools.split("\n")
+        .filter(line => (line.length > 4))
+        .forEach((line) => {
+          let lat = (line.split(',')[1] * 1) || 0,
+              lng = (line.split(',')[0] * 1) || 0,
+              latlng = new google.maps.LatLng(lat, lng),
+              first_graders = (line.split(',')[2] * 1) || 0,
+              second_graders = (line.split(',')[3] * 1) || 0,
+              label = (first_graders ?
+                  (Math.round(second_graders / first_graders * 100) + '%')
+                  : 'X'
+              );
+          // text marker plz
+        });
     });
   });
 }
