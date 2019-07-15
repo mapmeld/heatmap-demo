a3e96a0e9f822529a9ac6a593f22107871a3278d
Nick Doiron <ndoiron@mapmeld.com>
2019-07-15 20:41:05 +0900

    view grades 1, 1b, 2b

--- school-map.js
+++ school-map.js
@@ -102,39 +102,69 @@ function initMap() {
       ]
     });
 
-    let crv = document.createElement('canvas');
-        crv.width = 18;
-        crv.height = 12;
-        ctx = crv.getContext('2d');
-        ctx.font = '11px sans-serif';
-        ctx.fillStyle = '#000';
-    fetch("school_points.csv").then(res => res.text()).then((schools) => {
+    fetch("school_pts.csv").then(res => res.text()).then((schools) => {
+      let lookup = {};
+
       schools.split("\n")
         .filter(line => (line.length > 4))
         .forEach((line) => {
           let lat = (line.split(',')[1] * 1) || 0,
               lng = (line.split(',')[0] * 1) || 0,
               latlng = new google.maps.LatLng(lat, lng),
-              first_graders = (line.split(',')[2] * 1) || 0,
-              second_graders = (line.split(',')[3] * 1) || 0,
-              label = (first_graders ?
-                  (Math.round(second_graders / first_graders * 100))
-                  : 'X'
-              );
-          if (first_graders && second_graders) {
-            ctx.clearRect(0, 0, crv.width, crv.height);
-            ctx.fillText(label, 0, 12);
-            new google.maps.Marker({
-              position: latlng,
-              map: map,
-              clickable: false,
-              icon: {
-                url: crv.toDataURL(),
-                size: new google.maps.Size(18, 12)
-              }
-            });
-          }
+              school_code = (line.split(',')[2] * 1) || 0;
+          lookup[school_code] = latlng;
         });
+
+      let grade = '1st_to_2nd';
+      if (window.location.href.indexOf('1b') > -1) {
+        grade = 'hs1_to_hs2';
+      } else if (window.location.href.indexOf('2b') > -1) {
+        grade = 'hs2_to_hs3';
+      }
+
+      fetch("school_perf_" + grade + ".csv").then(res => res.text()).then((perfs) => {
+        let crv = document.createElement('canvas');
+            crv.width = 18;
+            crv.height = 22;
+            ctx = crv.getContext('2d');
+            ctx.font = '11px sans-serif';
+            ctx.fillStyle = '#000';
+
+        perfs.split("\n")
+          .filter(line => (line.length > 4))
+          .forEach((line) => {
+            let school = line.split(',')[0],
+                original = line.split(',')[1] * 1,
+                continued = line.split(',')[2] * 1,
+                label = Math.round(continued / original * 100),
+                point = lookup[school];
+            if (point && label) {
+              ctx.clearRect(0, 0, crv.width, crv.height);
+              let color = 'rgb(255, 0, 0, 0.3)';
+              if (label > 50) {
+                color = 'rgb(255, 255, 0, 0.3)';
+              } else if (label > 70) {
+                color = 'rgb(0, 255, 0, 0.5)';
+              }
+              ctx.fillStyle = color;
+              ctx.arc(9, 6, 9, 0, 2 * Math.PI, false);
+              ctx.fill();
+              ctx.fillStyle = '#000';
+              let xval = Math.floor((crv.width - ctx.measureText(label).width) / 2);
+              ctx.fillText(label, xval, 10);
+              new google.maps.Marker({
+                position: point,
+                map: map,
+                clickable: false,
+                icon: {
+                  url: crv.toDataURL(),
+                  size: new google.maps.Size(18, 12)
+                }
+              });
+            }
+          });
+
+      });
     });
   });
 }
